d2de451a12ced95a5e523a515934a671d544dbdf
Morgassa <amadeudeassuncao@gmail.com>
2019-10-24 11:23:39 -0300

    Estou me divertindo!

--- (6 kyu) Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!.py
+++ (6 kyu) Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!.py
@@ -7,12 +7,11 @@ def sum_dig_pow(a, b):
         if int(number) <= 9:
             lista_resp.append(int(number))
         else:
-            x_lista = list(map(int,str(number)))
-            # print('check:', x_lista, lista_resp)
+            x_lista = list(map(int,str(number))) #CONVERTE AS SRINGS DE UMA LISTA PARA INTEIROS E JOGA NA LISTA 'X_LISTA'.
             for i in x_lista:
                 x_lista = list(map(lambda x: x, x_lista))
                 count2 =list(range(1,len(x_lista)+1))
-            resp = sum(map(lambda x, y: x ** y, x_lista, count2))
+            resp = sum(map(lambda x, y: x ** y, x_lista, count2)) #TRAZ A POTÊNCIA ENTRE DUAS LISTAS DIFERENTES E DEPOIS SOMA OS VALORES.
             count2 = []
             if number == resp:
                 lista_resp.append(resp)

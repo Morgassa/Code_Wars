a517ff6faea82c9bd4f9bf72b1f2254a36549f3e
Morgassa <amadeudeassuncao@gmail.com>
2019-11-08 12:09:26 -0300

    :) Just for Fun!

---
+++ (7 kyu) Simple Fun n74 Growing Plant.py
@@ -0,0 +1,15 @@
+def growing_plant(upSpeed, downSpeed, desiredHeight):
+    pass
+
+
+
+
+
+
+
+
+print(growing_plant(100,10,910))
+print(growing_plant(10,9,4))
+print(growing_plant(5,2,0))
+print(growing_plant(5,2,5))
+print(growing_plant(5,2,6))

395da1dfc7b71311c47fe29f194ee4b0b126e84f
Morgassa <amadeudeassuncao@gmail.com>
2019-11-12 01:08:57 -0300

    =D

---
+++ 7 kyu/(7kyu) Minimize Sum Of Array (Array Series #1).py
@@ -0,0 +1,11 @@
+def min_sum(arr):
+    lenght_array = list(range(0, len(arr)))
+    print (lenght_array)
+    # Your code here
+    pass
+
+
+
+print(min_sum([5,4,2,3]))
+print(min_sum([12,6,10,26,3,24]))
+print(min_sum([9,2,8,7,5,4,0,6]))
